a4dbce41480ceee3f0cb7698456ac76b827c179d
Teo <emaildeteo@gmail.com>
2021-07-21 09:27:57 +0200

    cleaned readme

--- bin/seed.js
+++ bin/seed.js
@@ -1,42 +1,28 @@
-require('dotenv').config();
-const mongoose     = require('mongoose');
-const Course = require('../models/Course');
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Course = require('../models/Course')
 
 const newCourse = {
- courseName : "WEB MAD OCT 19",
- students : [
-  {name:"Fran N.",codewars:"f-naranjo"},
-  {name:"Dani",codewars:"daniel.gonvie"},
-  {name:"Edu",codewars:"Eduardo290686"},
-  {name:"Manué",codewars:"Manuee97"},
-  {name:"Flor",codewars:"f!or"},
-  {name:"Javi",codewars:"Javiview"},
-  {name:"Kike",codewars:"enriqueMontano"},
-  {name:"Fran M.",codewars:"FranciscoMolleda"},
-  {name:"Héctor",codewars:"HectorArrieta"},
-  {name:"Estefanía",codewars:"estefania-git"},
-  {name:"Gabriel",codewars:"gabzafra"},
-  {name:"Laura",codewars:"laudvg"},
-  {name:"María",codewars:"mariasimo"},
-  {name:"Alfredo",codewars:"adelcanto"},
-  {name:"Sonia",codewars:"SoniaCB"},
-]}
+	courseName: 'WEB MAY 2021',
+	students: [
+		{ name: 'Name', codewars: 'codewars-username' },
+	]
+}
 
 console.log(process.env.MONGO)
 mongoose
-  .connect(`${process.env.MONGO}`, {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-    Course.create(newCourse)
-    .then((response)=>{
-      console.log(response)
-      mongoose.disconnect()
-    })
-    .catch((err)=>{console.log(err)})
-
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
-
+	.connect(`mongodb://localhost:27017/katas`, { useNewUrlParser: true })
+	.then(x => Course.find({ courseName: newCourse.courseName }))
+	.then(x => {
+		if (x.length) {
+			return Course.findOneAndRemove({ courseName: newCourse.courseName })
+		}
+	})
+	.then(x => Course.create(newCourse))
+	.then(response => {
+		console.log(response)
+		mongoose.disconnect()
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err)
+	})
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,62 @@
-const express = require('express');
-const Course = require('../models/Course');
-const router = express.Router();
-const axios = require('axios');
-const kata = "https://www.codewars.com/api/v1/code-challenges/";
+const express = require('express')
+const Course = require('../models/Course')
+const router = express.Router()
+const axios = require('axios')
+const kata = 'https://www.codewars.com/api/v1/code-challenges/'
 
 router.get('/', (req, res, next) => {
-  Course.find({})
-    .then(data => {
-      res.render('index', { data });
-    })
-});
+	Course.find({}).then(data => {
+		res.render('index', { data })
+	})
+})
 
 router.post('/result', (req, res, next) => {
-  Course.findOne({ courseName: req.body.Course })
-  .then((course) => {
-      axios.get(`${kata}${req.body.kataId}`)
-        .then((dataKata) => {
-          Promise.all(course.students.map((student) => {
-            return axios.get(`https://www.codewars.com/api/v1/users/${student.codewars}/code-challenges/completed?page=0`)
-              .then((dataUser) => {
-                const user = { name: student.name }
-                user.katas = dataUser.data.data
-                return user
-              })
-              .catch((err) => {
-                console.log( student)
-              })
-          }))
-            .then(arrUser => {
-              const result = arrUser.filter(user => {
-                return user.katas.some(eachKata =>
-                  eachKata.id == dataKata.data.id
-                )
-              })
-              if(result.length>0){
-                result[Math.floor(Math.random() * result.length)].pass = true
-              }
-              let statistics = {
-                students: course.students.length,
-                passOk: result.length,
-                percentage: 0
-              }
-              statistics.percentage = parseFloat((100/statistics.students)*statistics.passOk).toFixed(2)
-              res.render("result", { kataName: dataKata.data.name, result, statistics })
-            })
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    })
-    .catch((err) => { console.log(err) })
-});
+	Course.findOne({ courseName: req.body.Course })
+		.then(course => {
+			axios
+				.get(`${kata}${req.body.kataId}`)
+				.then(dataKata => {
+					Promise.all(
+						course.students.map(student => {
+							return axios
+								.get(
+									`https://www.codewars.com/api/v1/users/${student.codewars}/code-challenges/completed?page=0`
+								)
+								.then(dataUser => {
+									const user = { name: student.name }
+									user.katas = dataUser.data.data
+									return user
+								})
+								.catch(err => {
+									console.error(student, err)
+								})
+						})
+					).then(arrUser => {
+						const result = arrUser.filter(user => {
+							console.log(user)
+							return user.katas.some(eachKata => eachKata.id == dataKata.data.id)
+						})
+						if (result.length > 0) {
+							result[Math.floor(Math.random() * result.length)].pass = true
+						}
+						let statistics = {
+							students: course.students.length,
+							passOk: result.length,
+							percentage: 0
+						}
+						statistics.percentage = parseFloat(
+							(100 / statistics.students) * statistics.passOk
+						).toFixed(2)
+						res.render('result', { kataName: dataKata.data.name, result, statistics })
+					})
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
 
-module.exports = router;
+module.exports = router
